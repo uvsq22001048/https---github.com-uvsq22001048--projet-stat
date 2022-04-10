e475c0998abfa2a6afcebed266136bea40198bfe
Ceylia dupin <ceylia.dupin@ens.uvsq.fr>
2022-04-10 16:52:46 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# -projet-stat
+##############
+# Groupe 3 -- projet "statistique"
+# Ceylia DUPIN 
+# Louis REBECHE
+# https://github.com/uvsq22001048/-projet-stat
+##############
---
+++ projet_stat.py
@@ -0,0 +1,20 @@
+##############
+# Groupe 3 -- projet "statistique"
+# Ceylia DUPIN 
+# Louis REBECHE
+# https://github.com/uvsq22001048/-projet-stat
+##############
+
+#import des librairies 
+import tkinter as tk
+import random as rd
+
+# définition des widgets
+racine = tk.Tk()
+racine.title("fenetre stat")
+canvas = tk.Canvas(racine, width=600, height=600, bg="white")
+canvas.grid()
+
+
+# boucle principale
+racine.mainloop()
