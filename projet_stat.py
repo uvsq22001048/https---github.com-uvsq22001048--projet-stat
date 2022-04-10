c4b39fd4a93a74df652c0d56bd4cf29ec65b56b6
Ceylia dupin <ceylia.dupin@ens.uvsq.fr>
2022-04-10 17:11:37 +0200

    changement

--- projet_stat.py
+++ projet_stat.py
@@ -13,8 +13,13 @@ import random as rd
 racine = tk.Tk()
 racine.title("fenetre stat")
 canvas = tk.Canvas(racine, width=600, height=600, bg="white")
-canvas.grid()
-
+bouton_trace = tk.Button(racine, text="tracer la droite")
+bouton_color = tk.Button(racine, text="autre couleur")
+bouton_quitter = tk.Button(racine, text="quitter", command = quit)
+canvas.grid(column=1, row=0, rowspan=10)
+bouton_trace.grid(row=0)
+bouton_color.grid(row=1)
+bouton_quitter.grid(row=2)
 
 # boucle principale
 racine.mainloop()
